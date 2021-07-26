dcde601f55743c715c7c1be3fb89b775e342c1ff
Derek S <dschwan21@me.com>
2021-07-25 21:52:10 -0400

    added rest of the files to finish the lesson

--- lesson-2/arrays.c
+++ lesson-2/arrays.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 //implementation of array in c
+
+const TOTAL = 3;
+
+int main(void)
+{
+    int scores[TOTAL];
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length, i++)
+    {
+        sum = sum + array[]
+    }
+}
